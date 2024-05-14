f52006fd2c09d4ce83e8316388052693c9e8d80a
rfidparkingbitch <123532247+rfidparkingbitch@users.noreply.github.com>
2024-05-15 00:32:59 +0800

    beginning

---
+++ water_level_system/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center mt-[40px]">
+    <h1 class=" font-poppins text-[30px]">Water Level</h1>
+    <p class=" font-poppins font-bold text-[50px]">341</p>
+    <p class=" font-poppins font-bold text-[30px]">High</p>
+  </div>
+</template>
